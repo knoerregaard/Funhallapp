ce9809f790a93f5c634c0ee270727bf16bf54a76
Klaus <knoerregaard@gmail.com>
2019-04-11 11:08:22 +0200

    Update to klausview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { BookingListViewComponent } from './booking-list-view/booking-list-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'welcome',
+    component : WelcomeComponent
+  },
+  {
+    path : 'about',
+    component : AboutComponent
+  },
+  {
+    path : 'bookings',
+    component : BookingListViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/booking-list-view/booking-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-list-view',
+  templateUrl: './booking-list-view.component.html',
+  styleUrls: ['./booking-list-view.component.css']
+})
+export class BookingListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
