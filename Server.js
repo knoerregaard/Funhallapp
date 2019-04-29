44b9388f41f9fc8011859ff4122e0c23f6558bf3
Christian Guldager <chri707i@student.eadania.dk>
2019-04-29 13:04:08 +0200

    server

---
+++ Server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT =[3000, process.env.PORT];
+
+app.use(cors());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/Bookings', (req, res) => res.status(200).send({hej: 'verden'}));
+
+app.get('/test', (req, res) => res.status(200).send('TestTes'));
+
+app.post('/neworder', (req, res) =>{
+    console.log(req);
+    res.send("tak");
+})
+app.listen(PORT[0], () => console.log(`Example app`));
---
+++ src/app/fun.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class FunService {
+url = "http://localhost:3000";
+
+
+
+
+  constructor(private httpClient : HttpClient) { }
+  public getBookings(){
+    return this.httpClient.get(this.url + '/Bookings');
+  }
+//public newBooking(){
+//  return this.httpClient.post()
+//}
+} 
