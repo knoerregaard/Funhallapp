b032c59914e150e6c04951d954145aeec17c538f
Newjan Thevarajah <newj0001@student.eadania.dk>
2019-04-30 18:58:44 +0200

    cors

--- src/app/bookings.service.ts
+++ src/app/bookings.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingsService {
-
   constructor(private httpClient: HttpClient) { }
-
-  public getBookings(){
-    return this.httpClient.get('localhost:3000' + '/Bookings')
+  getBaseUrl(){
+    return 'http://localhost:3000';
+  }
+  getBookings() : Observable<string> {
+    const headers = new HttpHeaders({'Content-Type': 'text/plain'});
+    return this.httpClient.get('/Bookings', {responseType: 'text', headers});
   }
 }
